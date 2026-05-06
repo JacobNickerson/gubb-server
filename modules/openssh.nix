6b21f0864c59523233a4285c8a8bf0556faa204d
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-05-06 00:55:53 -0400

    Setup SSH

---
+++ modules/openssh.nix
@@ -0,0 +1,29 @@
+{ config, ... }:
+{
+	services.openssh = {
+		enable = true;
+		ports = [ 42067 ];
+		settings = {
+			PasswordAuthentication = false;
+			KbdInteractiveAuthentication = false;
+			PermitRootLogin = "no";
+			AllowUsers = null;
+			UseDns = true;
+			X11Forwarding = false;
+		};
+	};
+
+	services.endlessh = {
+		enable = true;
+		port = 22;
+		openFirewall = true;
+	};
+	
+	services.fail2ban = {
+		enable = true;
+	};
+
+	users.users."jacobnickerson".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOGsgM2ftNlPvn8Vltsj3+WuHhVZHMIW+5Iqk2ajfXJi jacobnickerson@PortaJake"
+	];
+}
