4f4b9e1fd0a57a6ee4a306e222a0d3eded7c64af
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-05-10 02:22:18 -0400

    Switch to zsh

--- configuration.nix
+++ configuration.nix
@@ -57,6 +57,7 @@
     EDITOR = "vim";
     VISUAL = "vim";
   };
+  environment.pathsToLink = [ "/share/zsh" ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- users/jacobnickerson.nix
+++ users/jacobnickerson.nix
@@ -3,18 +3,19 @@
 	home_dir = "/home/${user_name}";
   flake_path = "${home_dir}/gubb-server";
 	imports = [
-		./modules/fish.nix
 		./modules/git.nix
 		./modules/neovim.nix
 		(import ./modules/nix-helper.nix { flake_path = flake_path; })
 		./modules/tmux.nix
+		./modules/zsh.nix
 	];
 in {
 	config.users.users.jacobnickerson = {
 		isNormalUser = true;
 		description = "Jacob Nickerson";
 		extraGroups = [ "networkmanager" "wheel" "smb" ];
-		shell = pkgs.fish;
+		shell = pkgs.zsh;
+		ignoreShellProgramCheck = true; # NOTE: Silences a warning about shell not being enabled
 		packages = with pkgs; [];
 	};
 	config.home-manager.users.jacobnickerson = {
@@ -40,9 +41,10 @@ in {
 				la   = "eza -a --color=always --group-directories-first --icons";
 				ll   = "eza -l --color=always --group-directories-first --icons";
 				lt   = "eza -aT --color=always --group-directories-first --icons";
+				lg   = "eza -alg --color=always --group-directories-first --icons";
 				ldot = "eza -a | grep -e '^\\.'";
-				dev         = "nix develop --command fish";
-				tmp         = "nix-shell --command fish -p";
+				dev         = "nix develop --command zsh";
+				tmp         = "nix-shell --command zsh -p";
 				tarnow      = "tar -acf ";
 				untar       = "tar -zxvf ";
 				wget        = "wget -c ";
@@ -51,12 +53,7 @@ in {
 				dir         = "dir --color=auto";
 				vdir        = "vdir --color=auto";
 				grep        = "grep --color=auto";
-				fgrep       = "fgrep --color=auto";
-				egrep       = "egrep --color=auto";
-				hw          = "hwinfo --short";
-				big         = "expac -H M '%m\t%n' | sort -h | nl";
 				jctl        = "journalctl -p 3 -xb";
-				rip         = "expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl";
 			};
 		};
 
