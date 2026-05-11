5185f63a8ee7e899dd499b226b6dc4034f938626
Jacob Nickerson <jacobmilesnickerson@gmail.com>
2026-05-11 00:33:39 -0400

    Improvements to setup script

--- setup.sh
+++ setup.sh
@@ -13,13 +13,21 @@ if [ ! -d /storage ]; then
   exit 1
 fi
 
-mkdir -p /mnt
+echo "Generating secure boot keys..."
+nix-shell -p sbctl --command "sbctl create-keys"
+if [ ! -d /var/lib/sbctl/keys ]; then
+  echo "error: failed to generate secure boot keys"
+  exit 1
+fi
 
+echo "Creating swap subvolume..."
+mkdir -p /mnt
 mount $1 /mnt
 btrfs subvolume create /mnt/@swap
 umount /mnt
 mkdir -p /swap
 mount $1 /swap -o subvol=@swap
 
+echo "Building system..."
 nixos-generate-config --show-hardware-config > hardware-configuration.nix
 nixos-rebuild boot --flake .#GubbServer
